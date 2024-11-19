7a8a81c4dad3369dce7b4e88897f3bae0cb6f254
Anuska <anuskasth2022@gmail.com>
2024-11-19 11:18:15 +1100

    gti push

--- script.js
+++ script.js
@@ -1,78 +1,12 @@
+const apiURL = "https://randomuser.me/api?results=2";
+
+//unlock slider
 const slider = document.getElementById("slider");
 
-const contactList = [
-  {
-    gender: "male",
-    name: { title: "Mr", first: "Ryder", last: "Mitchell" },
-    location: {
-      street: { number: 4696, name: "3rd St" },
-      city: "Borden",
-      state: "Saskatchewan",
-      country: "Canada",
-      postcode: "I0J 6E1",
-      coordinates: { latitude: "-70.3394", longitude: "-17.7147" },
-      timezone: { offset: "+9:30", description: "Adelaide, Darwin" },
-    },
-    email: "ryder.mitchell@example.com",
-    login: {
-      uuid: "dae711e4-a50e-4c3f-8927-da048ab94703",
-      username: "blackladybug838",
-      password: "munch",
-      salt: "Qwb3Vp7r",
-      md5: "0bc06407daa1583e4f7e0ce70d69090f",
-      sha1: "cb4d7c6671e0490fbef17803c81f1d967aca919b",
-      sha256:
-        "ebcdc6b7acd192f58aaa4c4aa713754e2b635ee971f2d2ed95a754a409a7777e",
-    },
-    dob: { date: "1982-06-05T01:44:34.053Z", age: 42 },
-    registered: { date: "2002-10-19T05:06:11.886Z", age: 22 },
-    phone: "O67 P24-0234",
-    cell: "U95 D06-4566",
-    id: { name: "SIN", value: "992675397" },
-    picture: {
-      large: "https://randomuser.me/api/portraits/men/30.jpg",
-      medium: "https://randomuser.me/api/portraits/med/men/30.jpg",
-      thumbnail: "https://randomuser.me/api/portraits/thumb/men/30.jpg",
-    },
-    nat: "CA",
-  },
-  {
-    gender: "female",
-    name: { title: "Ms", first: "Magdalena", last: "Cvejić" },
-    location: {
-      street: { number: 9365, name: "Mažićka" },
-      city: "Kraljevo",
-      state: "Podunavlje",
-      country: "Serbia",
-      postcode: 19806,
-      coordinates: { latitude: "-27.4618", longitude: "-19.1072" },
-      timezone: { offset: "-10:00", description: "Hawaii" },
-    },
-    email: "magdalena.cvejic@example.com",
-    login: {
-      uuid: "ce810b69-abff-43ab-85fc-505b31c871f1",
-      username: "goldenswan976",
-      password: "louie",
-      salt: "PQ1CmpWD",
-      md5: "a39d30dd73218552fa040c7af6e4d539",
-      sha1: "fc9ff9ec6e0a3fbdcf2d0ab4fc471444fc505faf",
-      sha256:
-        "106c8c73d591e2f083236cb4eb932dca81fdb4bf06b8f25853f2f63a2b488fe0",
-    },
-    dob: { date: "1972-04-01T01:35:41.966Z", age: 52 },
-    registered: { date: "2014-03-21T11:55:36.395Z", age: 10 },
-    phone: "024-7330-550",
-    cell: "065-6145-251",
-    id: { name: "SID", value: "494404828" },
-    picture: {
-      large: "https://randomuser.me/api/portraits/women/10.jpg",
-      medium: "https://randomuser.me/api/portraits/med/women/10.jpg",
-      thumbnail: "https://randomuser.me/api/portraits/thumb/women/10.jpg",
-    },
-    nat: "RS",
-  },
-];
+//initial contact list array
+let contactList = [];
 
+//add change event trigger for slider value change
 slider.addEventListener("change", (e) => {
   const { value } = e.target;
 
@@ -83,6 +17,7 @@ slider.addEventListener("change", (e) => {
   }
 });
 
+//general function to display screen
 const displayScreen = (screenName) => {
   const screens = document.getElementsByClassName("screen");
 
@@ -94,7 +29,7 @@ const displayScreen = (screenName) => {
   }
 };
 
-//display contact count
+//display contact contact list
 
 const displaycontactList = (userList) => {
   const userNumber = userList.length;
@@ -172,6 +107,21 @@ const displayAppScreen = () => {
   displayScreen("appScreen");
 };
 
-displayScreen("contactListScreen");
+// to display conatct list + populate contact lsit
 
-displaycontactList(contactList);
+const displayContactListScreen = async () => {
+  displayScreen("contactListScreen");
+
+  const response = await fetch(apiURL);
+
+  const data = await response.json();
+  console.log(1000, data.results);
+  contactList = data.results;
+
+  // populate contact list
+
+  displaycontactList(contactList);
+};
+
+// displayScreen("contactListScreen");
+// displaycontactList(contactList);
